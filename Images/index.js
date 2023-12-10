a9be5980065a7350824420f19aacc8dd5b20b67c
Chaitanya Deshmukh <96743681+ChaitanyaD48@users.noreply.github.com>
2023-12-10 07:25:37 +0530

    Images Added

---
+++ Images/index.js
@@ -0,0 +1,13 @@
+import getShipment from "./2.jpg";
+import compShipment from "./3.jpg";
+import startShipment from "./4.jpg";
+import userProfile from "./5.jpg";
+import avatar from "./creator9.png";
+
+export default {
+  getShipment,
+  compShipment,
+  startShipment,
+  userProfile,
+  avatar,
+};
